1a4a5dc765ba4d48e7e6881f0c394409b92c3e1d
“[kainat <[kainathafeez04@gmail.com]>
2024-01-15 18:20:24 +0500

    change

---
+++ app.js
@@ -0,0 +1,79 @@
+// VARIABLES
+document.write("<h1>Javasrcipt variable</h1>")
+
+console.log("hello")
+var a = 67
+console.log(a)
+
+var b = 10
+var c = 20
+console.log(b+c)
+document.write("javasricpt variables" )
+document.write("<br>")
+
+document.write(b+c)
+document.write("<br>")
+var d = 20
+var e = 5
+document.write("variable value :",d+e)
+document.write("<br>")
+
+// javasrcipt data type
+document.write("<h1>Javasrcipt data type</h1>")
+
+var num = 10; 
+document.write("Data type" + (typeof num));
+document.write("<br>")
+
+var num = "10"; 
+document.write("Data type" + (typeof num));
+document.write("<br>")
+
+var num = true; 
+document.write("Data type" + (typeof num));
+document.write("<br>")
+
+var num = undefined; 
+document.write("Data type" + (typeof num));
+document.write("<br>")
+
+num = [1,2,3,4,5,6,false,"string"]
+document.write("Data type" + (typeof num));
+document.write("<br>")
+
+var num = {
+    aiman : 1,
+    kainat : 2,
+
+}
+document.write("Data type" + (typeof num));
+document.write("<br>")
+
+
+document.write("<br>")
+var stmarks = {
+    aiman : '1',
+    kainat : '2',
+}
+function findname(){
+
+}
+document.write(typeof findname);
+document.write("<br>")
+
+document.write("<br>")
+document.write("<h1>Add html tags in javasrcipt</h1>")
+document.write("<br>")
+
+
+document.write("How are you <br>")
+document.write("<b>How are you</b>")
+document.write("<br>")
+
+document.write("<u>How are you</u>")
+
+
+
+
+
+
